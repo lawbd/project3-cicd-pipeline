d3528bbd6b3b600140f9e4701d1ade0bf9815e8a
student <brian.law2@ngc.com>
2021-06-21 07:24:07 +0000

    update3 to Project3ApplicationTests.java

--- src/test/java/com/simplilearn/project3/Project3ApplicationTests.java
+++ src/test/java/com/simplilearn/project3/Project3ApplicationTests.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
+/*
 ##@SpringBootTest
 ##class Project3ApplicationTests {
 
@@ -21,7 +21,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 ##	}
 
 ##}
-
+*/
 
 /**
  * Created by 
